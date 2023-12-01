0604bfa4557a9484a31e340778bf6c63f1e491c2
unknown <Minh Hoang>
2023-12-01 17:11:32 +0700

    Tao model topics

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import * as database from "./config/database";
 import dotenv from "dotenv";
+import Topic from "./models/topic.model";
 
 dotenv.config();
 
@@ -15,7 +16,11 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 // end using pug
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false,
+  });
+  console.log(topics);
   // mặc định res.render đang trong view
   res.render("client/pages/topics/index");
 });
