d61bab6a8a61fefd5b4403573a939b24caece3af
unknown <Minh Hoang>
2023-12-01 17:26:35 +0700

    Tao controllers folder

---
+++ controllers/client/topic.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import Topic from "../../models/topic.model";
+
+// [GET /topics
+export const index = async (req: Request, res: Response): Promise<void> => {
+  const topics = await Topic.find({
+    deleted: false,
+  });
+  console.log(topics);
+  // mặc định res.render đang trong view
+  res.render("client/pages/topics/index");
+};
--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -1,15 +1,8 @@
-import { Request, Response, Router } from "express";
-import Topic from "../../models/topic.model";
+import { Router } from "express";
+import * as controller from "../../controllers/client/topic.controller";
 const router: Router = Router();
 
 // [GET] /topics
-router.get("/", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false,
-  });
-  console.log(topics);
-  // mặc định res.render đang trong view
-  res.render("client/pages/topics/index");
-});
+router.get("/", controller.index);
 
 export const topicRoutes = router;
