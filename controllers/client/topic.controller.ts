4e0854bd7ff5303f136cc93466c33b8817312cd3
unknown <Minh Hoang>
2023-12-02 11:10:20 +0700

    Trang chu de bai hat

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -5,10 +5,12 @@ import Topic from "../../models/topic.model";
 export const index = async (req: Request, res: Response): Promise<void> => {
   const topics = await Topic.find({
     deleted: false,
+    status: "active",
   });
-  console.log(topics);
+
   // mặc định res.render đang trong view
   res.render("client/pages/topics/index", {
     pageTitle: "Chủ đề bài hát",
+    topics: topics,
   });
 };
