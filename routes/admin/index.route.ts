715ec734857c42c15e1f97f590e3426deb7a83fb
Nguyen Minh Hoang <nemcabong@gmail.com>
2023-12-09 16:33:06 +0700

    Upload anh qua tinymce

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -3,12 +3,14 @@ import { dashboardRoutes } from "./dashboard.route";
 import { systemConfig } from "../../config/system";
 import { topicRoutes } from "./topic.route";
 import { songRoutes } from "./song.route";
+import { uploadRoutes } from "./upload.route";
 
 const adminRoutes = (app: Express): void => {
   const adminPath: string = `/${systemConfig.prefixAdmin}`;
 
   app.use(`${adminPath}/songs`, songRoutes);
   app.use(`${adminPath}/topics`, topicRoutes);
+  app.use(`${adminPath}/upload`, uploadRoutes);
   app.use(`${adminPath}/dashboard`, dashboardRoutes);
 };
 export default adminRoutes;
