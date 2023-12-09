7fbd287795b810a7dfb9e63941c357691719f312
Nguyen Minh Hoang <nemcabong@gmail.com>
2023-12-09 14:32:51 +0700

    Preview audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -12,3 +12,24 @@ if (uploadImage) {
   });
 }
 // End preview image.
+
+// Preview audio
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio) {
+  const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+  const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+
+  uploadAudioInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const audio = URL.createObjectURL(file);
+      const source = uploadAudioPlay.querySelector("source");
+      source.src = audio;
+
+      // khác với file image
+      // file audio cần phải gọi đến hàm load để load file
+      uploadAudioPlay.load();
+    }
+  });
+}
+// End preview audio
